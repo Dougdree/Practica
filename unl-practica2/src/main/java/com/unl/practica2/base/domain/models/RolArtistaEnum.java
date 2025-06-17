7ebe0a38b03471beb5f934f15ff04bc466b47bdd
Dougdree <douglas.carreno@unl.edu.ec>
2025-06-16 21:30:51 -0500

    Delete unl-practica2 directory
    Se me daño la primera carpeta entonces subire la que tiene hasta la mas reciente practica.

--- unl-practica2/src/main/java/com/unl/practica2/base/domain/models/RolArtistaEnum.java
+++
@@ -1,7 +0,0 @@
-package com.unl.practica2.base.domain.models;
-
-public enum RolArtistaEnum {
-    SOLISTA, VOCALISTA, BATERISTA, BAJISTA, GUITARRSITA;
-    
-}
-
--- unl-practica2/src/main/java/com/unl/practica2/base/domain/models/TipoArchivoEnum.java
+++
@@ -1,6 +0,0 @@
-package com.unl.practica2.base.domain.models;
-
-public enum TipoArchivoEnum {
-    FISICO,
-    VIRTUAL;
-}
